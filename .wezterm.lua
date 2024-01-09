a4d2f4042e0c327ef232c5460607878833b01536
Jordi Calafat <jcalafat97@gmail.com>
2024-01-09 19:56:03 +0100

    add some keys investigate for further commands

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,10 +4,34 @@ return {
   color_scheme = 'Catppuccin Mocha',
   font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Bold' }),
   font_size = 12.0, -- 18 for recording
-  enable_tab_bar = false,
   cursor_blink_rate = 750,
+  audible_bell = 'Disabled',
+
+  enable_tab_bar = false,
   macos_window_background_blur = 30,
   window_background_opacity = 0.95,
   window_decorations = 'RESIZE',
-  audible_bell = 'Disabled',
+
+  keys = {
+    {
+      key = 'm',
+      mods = 'CMD',
+      action = wezterm.action.DisableDefaultAssignment,
+    },
+    -- {
+    --   key = 'f',
+    --   mods = 'CTRL',
+    --   action = wezterm.action.SendKey({ key = 'RightArrow' }),
+    -- },
+    -- {
+    --   key = 'p',
+    --   mods = 'CTRL',
+    --   action = wezterm.action.SendKey({ key = 'UpArrow' }),
+    -- },
+    -- {
+    --   key = 'n',
+    --   mods = 'CTRL',
+    --   action = wezterm.action.SendKey({ key = 'DownArrow' }),
+    -- },
+  },
 }
