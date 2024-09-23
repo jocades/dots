80b8222356846af2e4c024b538c425632bcbfd71
Jordi Calafat <jcalafat97@gmail.com>
2024-09-23 16:36:25 +0200

    module for programs

--- home/default.nix
+++ home/default.nix
@@ -21,36 +21,5 @@
     };
   };
 
-  programs = {
-    atuin = {
-      enable = true;
-      enableZshIntegration = true;
-      flags = [ "--disable-up-arrow" ];
-      settings = {
-        style = "compact";
-        keymap_mode = "auto";
-      };
-    };
-
-    direnv = {
-      enable = true;
-      enableZshIntegration = true;
-      # nix-direnv.enable = true;
-    };
-
-    fzf = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-
-    pyenv = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-
-    zoxide = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-  };
+  programs = import ./programs.nix { };
 }
---
+++ home/programs.nix
@@ -0,0 +1,33 @@
+{ ... }:
+{
+  atuin = {
+    enable = true;
+    enableZshIntegration = true;
+    flags = [ "--disable-up-arrow" ];
+    settings = {
+      style = "compact";
+      keymap_mode = "auto";
+    };
+  };
+
+  direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    # nix-direnv.enable = true;
+  };
+
+  fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  pyenv = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+}
