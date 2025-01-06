61ae6806045b1ee4229d426327353e6c756ccae3
Jordi Calafat <104111071+jocades@users.noreply.github.com>
2025-01-06 09:23:04 +0100

    bitsc++ and rustfmt

---
+++ home/stdc++.h
@@ -0,0 +1,30 @@
+#ifndef STDC_PLUS_PLUS_H
+#define STDC_PLUS_PLUS_H
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#define all(x) begin(x), end(x)
+#define iter(i, a, b) for (int i = a; i < (b); i++)
+#define loop while (true)
+#define sz(x) (int)x.size()
+#define cts(a, b, c) (a <= c && c < b)
+
+#endif
